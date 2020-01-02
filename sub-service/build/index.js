d4baf8040517ce7e30ccf5ea3a3f124db4d3c51b
robin2017 <robin.seu@foxmail.com>
2020-01-02 23:46:02 +0800

    初步构建

--- sub-service/build/index.js
+++ sub-service/build/index.js
@@ -2,7 +2,8 @@
  * 将模块构建成ws组件
  * */
 const path = require('path');
-const child_process = require('child_process')
+const child_process = require('child_process');
+const rimraf = require('rimraf');
 
 //获取package.json中信息
 const msModules = require(path.resolve(__dirname, '../package.json'))['micro-service-modules'];
@@ -10,17 +11,26 @@ if (!msModules instanceof Array) {
     console.log('请填写[micro-service-modules]属性');
     return;
 }
-msModules.forEach(item => {
-    const {module, name} = item;
-    if (!module || !name) {
-        console.log('模块中必须有module&name属性');
-        return
+// 删除当前目录下的packages目录
+rimraf(path.resolve(__dirname, '../packages'), function (err) {
+    if (err) {
+        console.log(err);
+        return;
     }
 
-    const cmd = `vue-cli-service build --dest packages/${module} --target wc --name ms-${name} src/modules/${module}/Index.vue`;
-    console.log(cmd);
-    child_process.exec(cmd, function (err) {
-        if (err) console.error(err);
-        console.log(`模块[${module}]构建成功！`)
+    msModules.forEach(item => {
+        const {module, name} = item;
+        if (!module || !name) {
+            console.log('模块中必须有module&name属性');
+            return
+        }
+
+        const cmd = `vue-cli-service build --dest packages/${module} --target wc --name ms-${name} src/modules/${module}/Index.vue`;
+        console.log(cmd);
+        child_process.exec(cmd, function (err) {
+            if (err) console.error(err);
+            console.log(`模块[${module}]构建成功！`)
+        });
     });
-});
+})
+
