d3090ba560e32f352d1540d95add4848059200d0
robin2017 <robin.seu@foxmail.com>
2020-01-18 13:21:24 +0800

    可行方案

--- sub-service/vue.config.js
+++ sub-service/vue.config.js
@@ -8,8 +8,8 @@ const vueConf = {
     configureWebpack: config => {
         if (projectConf.target === 'wc' || projectConf.target === 'lib') {
             config.plugins.push(new CopyWebpackPlugin([{
-                from:path.resolve(__dirname,'./public/lib'),
-                to:path.resolve(__dirname,projectConf.packageName,'./lib')
+                from: path.resolve(__dirname, './public/lib'),
+                to: path.resolve(__dirname, projectConf.packageName, './lib')
             }]));
             let htmlWebpackPlugin = config.plugins.find(item => {
                 return item instanceof HtmlWebpackPlugin
@@ -18,28 +18,36 @@ const vueConf = {
                 htmlWebpackPlugin.options.template = path.resolve(__dirname,
                     `./public/index-${projectConf.target}.html`)
             }
-            // config.externals={
-            //     'vue':{
-            //         commonjs:'vue',
-            //         commonjs2:'vue',
-            //         umd:'vue',
-            //         root:'Vue'
-            //     }
-            // }
-            // config.optimization = {
-            //     splitChunks: {
-            //         cacheGroups: {
-            //             vue: {
-            //                 name: "chunk-vue",
-            //                 test: /[\\/]node_modules[\\/]vue[\\/]/,
-            //                 chunks: "initial",
-            //                 priority: 3,
-            //                 reuseExistingChunk: true,
-            //                 enforce: true
-            //             }
-            //         }
-            //     }
-            // }
+            config.externals = {
+                'vue': {
+                    commonjs: 'vue',
+                    commonjs2: 'vue',
+                    umd: 'vue',
+                    root: 'Vue'
+                }
+            }
+            config.optimization = {
+                splitChunks: {
+                    cacheGroups: {
+                        vue: {
+                            name: "chunk-vue",
+                            test: /[\\/]node_modules[\\/]vue[\\/]/,
+                            chunks: "initial",
+                            priority: 3,
+                            reuseExistingChunk: true,
+                            enforce: true
+                        },
+                        // elementUi: {
+                        //     name: "chunk-element-ui",
+                        //     test: /[\\/]node_modules[\\/]element-ui[\\/]/,
+                        //     chunks: "initial",
+                        //     priority: 3,
+                        //     reuseExistingChunk: true,
+                        //     enforce: true
+                        // }
+                    }
+                }
+            }
         }
     }
 };
