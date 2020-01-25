72cbafc608148f272e8b441f3a76e8a6d385b750
robin2017 <robin.seu@foxmail.com>
2020-01-25 23:33:56 +0800

    tmp suc

--- main-service/src/components/ShowAll.vue
+++ main-service/src/components/ShowAll.vue
@@ -1,7 +1,13 @@
 <template>
     <div>
         <h1>微服务展示</h1>
-        <ms-wc-table></ms-wc-table>
+        <div style="height:300px;width:600px;border:1px solid red;">
+            <ms-wc-table></ms-wc-table>
+        </div>
+        <div style="height:300px;width:600px;border:1px solid red;">
+            <ms-wc-tree></ms-wc-tree>
+        </div>
+
     </div>
 </template>
 
