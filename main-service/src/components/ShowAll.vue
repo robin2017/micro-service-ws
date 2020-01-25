346827887fe6bf40859f46fdcffe544b7e899b7e
robin2017 <robin.seu@foxmail.com>
2020-01-25 23:38:39 +0800

    tmp suc

--- main-service/src/components/ShowAll.vue
+++ main-service/src/components/ShowAll.vue
@@ -7,7 +7,12 @@
         <div style="height:300px;width:600px;border:1px solid red;">
             <ms-wc-tree></ms-wc-tree>
         </div>
-
+        <div style="height:300px;width:600px;border:1px solid red;">
+            <ms-wc-collapse></ms-wc-collapse>
+        </div>
+        <div style="height:300px;width:600px;border:1px solid red;">
+            <ms-wc-hello></ms-wc-hello>
+        </div>
     </div>
 </template>
 
