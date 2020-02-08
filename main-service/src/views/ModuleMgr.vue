3c748bfb96029d6c76814cc6470a1aea5ca0352c
robin2017 <robin.seu@foxmail.com>
2020-02-09 01:15:52 +0800

    df

--- main-service/src/views/ModuleMgr.vue
+++ main-service/src/views/ModuleMgr.vue
@@ -19,7 +19,7 @@
             </el-table-column>
             <el-table-column
                     label="操作"
-                    width="80">
+                    width="100">
                 <template slot-scope="scope">
                     <el-button @click="openUrl(scope.row.moduleUrl)" type="primary">打开</el-button>
                 </template>
