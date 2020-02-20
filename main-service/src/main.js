1db6d1a90f355348cb0a9bfd246ff0dbbd622e05
robin2017 <robin.seu@foxmail.com>
2020-02-20 20:59:01 +0800

    所有功能点全部完成

--- main-service/src/main.js
+++ main-service/src/main.js
@@ -9,7 +9,10 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
 
-
+//引入Antd
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+Vue.use(Antd);
 
 new Vue({
   router,
