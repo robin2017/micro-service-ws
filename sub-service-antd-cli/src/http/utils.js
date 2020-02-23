5dbfcb8732cac63bb8c459373c03151a9822c684
robin2017 <robin.seu@foxmail.com>
2020-02-23 19:34:28 +0800

    suc

--- sub-service-antd-cli/src/http/utils.js
+++ sub-service-antd-cli/src/http/utils.js
@@ -3,6 +3,8 @@ const ipPortReg = /http:\/\/(.*?)\//;
 export function getRuntimeBaseUrl() {
     const currentScript = window.document.currentScript;
     let baseUrl = "";
+    //这里经常报错，现在排查下
+    console.log('antd===getRuntimeBaseUrl===:currentScript',currentScript,currentScript.src)
     if (currentScript && currentScript.src) {
         const ret = ipPortReg.exec(currentScript.src);
         if (ret) {
